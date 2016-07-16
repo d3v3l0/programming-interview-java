6a0a8298d487def974ae7c0f7fd873576854509c
Oleksii Tkachuk <kelebra20@gmail.com>
2016-07-15 21:45:23 -0500

    Implemented reverse string, reverse string range, reverse words in sentence problems

--- src/main/java/com/github/tkachuko/origramming/interview/strings/Strings.java
+++ src/main/java/com/github/tkachuko/origramming/interview/strings/Strings.java
@@ -17,4 +17,55 @@ public class Strings {
         }
         return decoded;
     }
+
+    /**
+     * Reverse characters in a string
+     *
+     * @param string original string
+     * @return string with reversed characters
+     */
+    public static String reverse(String string) {
+        char[] characters = string.toCharArray();
+        reverseRange(characters, 0, characters.length - 1);
+        return new String(characters);
+    }
+
+    /**
+     * Reverses certain range in a string
+     *
+     * @param characters characters array
+     * @param start      start index of reversed substring
+     * @param end        end index of reversed substring
+     */
+    public static void reverseRange(char[] characters, int start, int end) {
+        for (int i = start, j = end; i <= j; i++, j--) {
+            char tmp = characters[i];
+            characters[i] = characters[j];
+            characters[j] = tmp;
+        }
+    }
+
+    /**
+     * Returns sentence with words in opposite order i.e bob is cool => cool is bob
+     *
+     * @param sentence sentence with words
+     * @return sentence with words in reverse order
+     */
+    public static String reverseWordsInSentence(String sentence) {
+        char[] characters = sentence.toCharArray();
+        reverseRange(characters, 0, characters.length - 1);
+        int wordStart = 0;
+        for (int i = 0; i < characters.length - 1; i++) {
+            if (Character.isSpaceChar(characters[i])) {
+                reverseRange(characters, wordStart, i - 1);
+                wordStart = i + 1;
+            }
+        }
+        if (wordStart != 0) {
+            reverseRange(characters, wordStart, characters.length - 1);
+        } else {
+            reverseRange(characters, 0, characters.length - 1);
+        }
+        return new String(characters);
+    }
 }
--- src/test/java/com/github/tkachuko/origramming/interview/strings/StringsTest.java
+++ src/test/java/com/github/tkachuko/origramming/interview/strings/StringsTest.java
@@ -5,7 +5,8 @@ import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static com.github.tkachuko.origramming.interview.strings.Strings.decodeExcelColumnNumber;
+import static com.github.tkachuko.origramming.interview.strings.Strings.*;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(JUnitParamsRunner.class)
@@ -22,6 +23,49 @@ public class StringsTest {
                 new Object[]{"A", 1},
                 new Object[]{"AA", 27},
                 new Object[]{"ZZ", 702}
+        };
+    }
+
+    @Test
+    @Parameters
+    public void shouldReverseString(String string, String reversed) {
+        assertEquals(reversed, reverse(string));
+    }
+
+    public Object parametersForShouldReverseString() {
+        return new Object[]{
+                new Object[]{"A", "A"},
+                new Object[]{"AB", "BA"},
+                new Object[]{"ZZABBZ123", "321ZBBAZZ"}
+        };
+    }
+
+    @Test
+    @Parameters
+    public void shouldReverseRangeInString(char[] string, int start, int end, char[] reversed) {
+        reverseRange(string, start, end);
+        assertArrayEquals(reversed, string);
+    }
+
+    public Object parametersForShouldReverseRangeInString() {
+        return new Object[]{
+                new Object[]{"ABSSA".toCharArray(), 0, 2, "SBASA".toCharArray()},
+                new Object[]{"ZZABBZ123".toCharArray(), 3, 5, "ZZAZBB123".toCharArray()}
+        };
+    }
+
+    @Test
+    @Parameters
+    public void shouldReverseWordsInSentence(String string, String wordsReversed) {
+        assertEquals(wordsReversed, reverseWordsInSentence(string));
+    }
+
+    public Object parametersForShouldReverseWordsInSentence() {
+        return new Object[]{
+                new Object[]{"Alice likes Bob", "Bob likes Alice"},
+                new Object[]{"Hello World", "World Hello"},
+                new Object[]{"SingleWord", "SingleWord"},
+                new Object[]{"Reverse Words In Sentence", "Sentence In Words Reverse"}
         };
     }
 }
