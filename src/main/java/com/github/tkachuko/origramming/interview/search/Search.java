b41deb009de94ebf229e9d3c569c5306d76daa62
Oleksii Tkachuk <kelebra20@gmail.com>
2016-11-18 23:42:49 -0600

    Implemented local minima search

--- src/main/java/com/github/tkachuko/origramming/interview/search/Search.java
+++ src/main/java/com/github/tkachuko/origramming/interview/search/Search.java
@@ -16,13 +16,36 @@ public class Search {
         while (start <= end) {
             int mid = start + (end - start) / 2;
             int current = list.get(mid);
-            if (current > element) end -= 1;
-            else if (current < element) start += 1;
+            if (current > element) end = mid - 1;
+            else if (current < element) start = mid + 1;
             else {
                 result = mid;
                 end = mid - 1;
             }
         }
         return result;
+    }
+
+    /**
+     * Finds local minima in list ascending from left to right and descending from right to left
+     *
+     * @param list of integers
+     * @return local minima
+     */
+    public static int localMinima(List<Integer> list) {
+        int start = 0, end = list.size() - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid == 0 || mid == list.size() - 1) return -1;
+
+            int minima = list.get(mid);
+            int left = list.get(mid - 1);
+            int right = list.get(mid + 1);
+
+            if (minima <= left && minima <= right) return minima;
+            else if (minima < right) end = mid;
+            else if (minima > left) start = mid;
+        }
+        return -1;
     }
 }
--- src/test/java/com/github/tkachuko/origramming/interview/search/SearchTest.java
+++ src/test/java/com/github/tkachuko/origramming/interview/search/SearchTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import java.util.List;
 
 import static com.github.tkachuko.origramming.interview.search.Search.indexOfInSorted;
+import static com.github.tkachuko.origramming.interview.search.Search.localMinima;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
@@ -29,6 +30,22 @@ public class SearchTest {
                 new Object[]{asList(1, 1, 2, 3, 3, 4, 5), 5},
                 new Object[]{asList(1, 1, 2, 3, 3, 4, 5), -1},
                 new Object[]{asList(1, 1, 2, 3, 3, 4, 5), 10}
+        };
+    }
+
+    @Test
+    @Parameters
+    public void shouldFindLocalMinimaIn(List<Integer> list, int minima) {
+        assertThat(localMinima(list)).isEqualTo(minima);
+    }
+
+    private static Object parametersForShouldFindLocalMinimaIn() {
+        return new Object[]{
+                new Object[]{asList(1, 3, 2, 4, 5), 2},
+                new Object[]{asList(1, 2, 3, 4, 5, 6), -1},
+                new Object[]{asList(1, 2, 3, 2, 5, 6, 7), 2},
+                new Object[]{asList(1, -8, 3, 5, 6), -8},
+                new Object[]{asList(1, -10, 3, 4, 5, 6), -10}
         };
     }
 }
