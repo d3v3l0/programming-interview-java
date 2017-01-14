500081df17aa5dbd60328162f22721bbc01423f7
Oleksii Tkachuk <kelebra20@gmail.com>
2017-01-13 22:38:36 -0600

    Implemented LRU cache

--- src/main/java/com/github/tkachuko/origramming/interview/tables/HashTables.java
+++ src/main/java/com/github/tkachuko/origramming/interview/tables/HashTables.java
@@ -67,4 +67,79 @@ public class HashTables {
         }
         return count.isEmpty();
     }
+
+    /**
+     * Generic LRU cache implementation with three operations: insert, lookup and erase
+     *
+     * @param <K> type of key
+     * @param <V> type of value
+     */
+    public static class LRUCache<K, V> {
+
+        private final Map<K, V> cache;
+
+        public LRUCache(int capacity) {
+            this.cache = new LinkedHashMap<K, V>() {
+                @Override
+                protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+                    return size() > capacity;
+                }
+            };
+        }
+
+        /**
+         * Inserts value for the key in case it is not present in the cache.
+         * If value is already in the cache mapping is not changed but is being put on top of the cache.
+         *
+         * @param key   in the cache
+         * @param value that should be inserted in case no mapping exists
+         */
+        public void insert(K key, V value) {
+            V currentValue = cache.get(key);
+            if (currentValue != null) moveToFront(key, currentValue);
+            else cache.put(key, value);
+        }
+
+        /**
+         * Get current value mapped to the given key as an Optional
+         *
+         * @param key in the cache
+         * @return optional that contains value mapped to the key in the cache
+         */
+        public Optional<V> lookup(K key) {
+            Optional<V> result = Optional.ofNullable(cache.get(key));
+            result.ifPresent(value -> moveToFront(key, value));
+            return result;
+        }
+
+        /**
+         * Removes mapping for the given key in the cache
+         *
+         * @param key to remove mapping of
+         */
+        public void erase(K key) {
+            cache.remove(key);
+        }
+
+        /**
+         * Moves entry so that it is becomes the most recently used and won't be deleted
+         *
+         * @param key   of the cache
+         * @param value associated with the key
+         */
+        private void moveToFront(K key, V value) {
+            cache.remove(key);
+            cache.put(key, value);
+        }
+    }
+
+    /**
+     * Creates ISBN book LRU cache where key is ISBN in integer form and value is price of the item
+     *
+     * @param capacity of the cache. Adding more items than capacity will make least used items be deleted.
+     * @return ISBN LRU cache with prices
+     */
+    public LRUCache<Integer, Integer> isbnCache(int capacity) {
+        return new LRUCache<>(capacity);
+    }
 }
